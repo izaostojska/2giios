5b9d45cb98e75962b105236865055f5ae864287d
izaostojska <147402977+izaostojska@users.noreply.github.com>
2023-11-13 15:43:52 +0100

    Add files via upload

---
+++ 2giios/2giios/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  2giios
+//
+//  Created by student on 13/11/2023.
+//  Copyright © 2023 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (nonatomic, weak) IBOutlet UIButton *informationButton;
+-(IBAction)Information;
+
+@property (nonatomic, weak) IBOutlet UIImageView *Banerpl;
+@property (nonatomic, weak) IBOutlet UIImageView *Baneren;
+@end
+
